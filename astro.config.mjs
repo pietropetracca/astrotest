c0497b25b9f918353fa116b2ca1379c6d5829e70
pietropetracca <pietro.petracca@issgreppi.it>
2025-08-29 19:16:36 +0200

    Update configuration for astrotest repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://pietro.petracca.github.io',
-	base: '/astro/',
+	base: '/astrotest/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
