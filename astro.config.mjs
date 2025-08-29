05158ff2798e3177b61dcae9c6d2a2350c47a586
pietropetracca <pietro.petracca@issgreppi.it>
2025-08-29 18:56:26 +0200

    Update base path for GitHub repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://pietro.petracca.github.io',
-	base: '/cosmic-corot/',
+	base: '/astro/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
