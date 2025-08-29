8f3b3075417adcd3f85683d0293067537d4e5025
pietropetracca <pietro.petracca@issgreppi.it>
2025-08-29 18:52:13 +0200

    Add GitHub Actions workflow for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://pietro.petracca.github.io',
+	base: '/cosmic-corot/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
